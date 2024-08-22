3a7bcc7db378bffb94c00aa90dac6df0f4bf635f
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-08-22 13:49:39 +0545

    Add home page data

---
+++ newspaper/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import ListView
+
+from newspaper.models import Category, Post, Tag
+
+from datetime import timedelta
+from django.utils import timezone
+
+class HomeView(ListView):
+    model = Post
+    template_name = "aznews/home.html"
+    context_object_name = "posts"
+    queryset = Post.objects.filter(published_at__isnull=False, status="active")[:4]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["featured_post"] = (
+            Post.objects.filter(published_at__isnull=False, status="active")
+            .order_by("-published_at", "-views_count")
+            .first()
+        )
+        context["featured_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at", "-views_count")[1:4]
+
+        one_week_ago = timezone.now() - timedelta(days=7)
+        context["weekly_top_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
+        ).order_by("-published_at", "-views_count")[:7]
+
+        context["recent_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:7]
+
+        context["categories"] = Category.objects.all()[:4]
+        context["tags"] = Tag.objects.all()[:4]
+
+        return context
